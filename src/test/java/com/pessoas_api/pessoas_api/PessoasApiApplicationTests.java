b4bd8d13195a461bb9a2c124dd938f0a51256804
Marcus Pará <marcuspara66@gmail.com>
2023-01-07 15:21:47 -0300

    initital commit

---
+++ src/test/java/com/pessoas_api/pessoas_api/PessoasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pessoas_api.pessoas_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PessoasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
